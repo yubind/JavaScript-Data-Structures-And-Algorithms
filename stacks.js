676e6cec100cc2219bbf25da30a8d5fb57c5e679
yubin <yubind91@gmail.com>
2019-03-26 19:19:08 -0700

    stacks

---
+++ stacks.js
@@ -0,0 +1,28 @@
+/* stacks */ 
+
+// functions: push, pop, peek, length 
+
+//palindrome
+
+var letters = []; //stack
+
+var word = 'racecar';
+
+var rword = '';
+
+//put letters of word into stack
+for(var i = 0; i < word.length; i++) {
+    letters.push(word[i]);
+}
+
+//pop off the stack in reverse order
+for(var i = 0; i < word.length; i++) {
+    rword += letters.pop();
+}
+
+//see if rword is the same as word
+if(rword === word) {
+    console.log(word + 'is a palindrome');
+} else {
+    console.log(word + 'is not a palindrome');
+}
